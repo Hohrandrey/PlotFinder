0672063acfbe754e861b13362c8a3e97a0682b39
Hohrandrey <EnderBro_2005@mail.ru>
2025-09-04 15:15:43 +0300

    изменение роутеров, Исправление бага
    -дописать функционал кнопки для управления участками
    
    (надо проверить, чтобы можно было вызывать только из конкретных мест в программе и как-то сделать чтобы кнопка назад возвращала всегда на прошлое окно)

--- resource/commands/__routers.py
+++ resource/commands/__routers.py
@@ -3,4 +3,4 @@ from .admin_commands.admin_start import router as admin_start_router
 from .buyer_commands.buyer_start import router as buyer_start_router
 from .admin_commands.user_management import router as user_management_router
 from .admin_commands.plot_management import router as plot_management
-from .admin_commands.user_management import router as сhanging_personal_data
+from .admin_commands.changing_personal_data import router as сhanging_personal_data
