a5c490c1387ae83abc6e2679a40e0e8ecca89e74
Hohrandrey <EnderBro_2005@mail.ru>
2025-09-05 09:46:31 +0300

    добавлены кнопки добавления и удаления участков
    -дописать функционал кнопки для управления участками
    -сделать чтобы кнопка назад возвращала всегда на прошлое окно
    -сделать так, чтобы кнопки запускались только из конкретных мест
    -new_first_name в first_name_changing сделать так, чтобы в этой переменной был ответ пользователя

--- resource/commands/admin_commands/plot_management.py
+++ resource/commands/admin_commands/plot_management.py
@@ -9,4 +9,6 @@ router = Router()
 
 @router.message(F.text == "Управление участками")
 async def command_start_handler(message: Message):
-    await message.answer(f'Меню управления участками', reply_markup = plot_management_kb) # сообщение и подключение клавиатуры
+    await message.answer(f'Меню управления участками') # сообщение
+
+    await message.answer(f'', reply_markup = plot_management_kb) # подключение клавиатуры
--- resource/keyboards/admin_kb/plot_management_kb.py
+++ resource/keyboards/admin_kb/plot_management_kb.py
@@ -1,4 +1,15 @@
 # клавиатура с просмотром участков для их модерирования
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
-plot_management_kb =  []
+kb = [
+    [KeyboardButton(text = 'Оставить участок')],
+    [KeyboardButton(text = 'Удалить участок')]
+]
+
+
+plot_management_kb = ReplyKeyboardMarkup(
+    keybord = kb,
+    resize_keyboard = True,
+    one_time_keyboard = True
+)
