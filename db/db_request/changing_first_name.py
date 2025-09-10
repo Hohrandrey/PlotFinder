3f8c367459f0bd87be90c54a990515fe07607f77
Hohrandrey <EnderBro_2005@mail.ru>
2025-09-10 19:49:28 +0300

    поменял фамилию на имя
    -дописать функционал кнопки для управления участками
    -сделать чтобы кнопка назад возвращала всегда на прошлое окно
    -сделать так, чтобы кнопки запускались только из конкретных мест
    -new_first_name в first_name_changing сделать так, чтобы в этой переменной был ответ пользователя

--- db/db_request/changing_first_name.py
+++ db/db_request/changing_first_name.py
@@ -6,7 +6,7 @@ def changing_first_name(new_first_name, username):
     connection = sqlite3.connect(db_name) # устанавливаем соединение с базой данных
     cursor = connection.cursor() # курсор для управления БД
 
-    # обновляем фамилию пользователя с нужным нам username
+    # обновляем имя пользователя с нужным нам username
     cursor.execute('UPDATE admins SET first_name = ? WHERE username = ?', (new_first_name, username))
 
     connection.commit() # сохранение изменений
--- resource/commands/admin_commands/first_name_changing.py
+++ resource/commands/admin_commands/first_name_changing.py
@@ -2,19 +2,20 @@ from aiogram import F
 from aiogram import Router
 from aiogram.types import Message
 from db.db_request.changing_first_name import changing_first_name
+from ..states import Form
 
 
 router = Router()
 
 
-@router.message(F.text == "Фамилия")
+@router.message(F.text == "Имя")
 async def changing_first_name(message: Message):
     """
-    Здесь реализация изменения фамилии в БД через db_request
+    Здесь реализация изменения Имени в БД через db_request
     """
     username = message.from_user.username
-    await message.answer("Введите новую фамилию")
+    await message.answer("Введите новое имя")
     new_first_name = message.from_user
     await message.answer(username)
     await changing_first_name(new_first_name, username)
-    await message.reply("Фамилия изменена")
+    await message.reply("Имя изменено")
