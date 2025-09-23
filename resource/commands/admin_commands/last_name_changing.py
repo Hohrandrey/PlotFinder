280ff036f6a68a302901ae601612b531127beb55
Hohrandrey <EnderBro_2005@mail.ru>
2025-09-23 12:50:56 +0300

    возвращение в меню и кнопка назад

--- resource/commands/admin_commands/last_name_changing.py
+++ resource/commands/admin_commands/last_name_changing.py
@@ -1,10 +1,12 @@
 from aiogram import F
+from ..states import Form
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from db.db_request.last_name_changing_db import last_name_changing_db
-from ..states import Form
-
+from ...keyboards.back_and_menu_button import back_and_menu_button
+from ...keyboards.admin_kb.admin_start_kb import admin_start_kb
+from ...keyboards.admin_kb.changing_personal_data_kb import changing_personal_data_kb
 
 router = Router()
 
@@ -24,3 +26,16 @@ async def process_new_last_name(message: Message, state: FSMContext):
     new_last_name = message.text.strip()
     last_name_changing_db(new_last_name, username)
     await message.answer(f"Фамилия изменена на: {new_last_name}")
+    await state.set_state(Form.back_and_menu)
+    await message.answer("Выберите действие", reply_markup=back_and_menu_button)
+
+
+@router.message(F.text == 'Назад', Form.back_and_menu)
+async def back_button(message: Message, state: FSMContext):
+    await state.set_state(Form.changing_personal_data)
+    await message.answer(f'Меню изменения персональных данных', reply_markup=changing_personal_data_kb)
+
+@router.message(F.text == 'В меню', Form.back_and_menu)
+async def menu_button(message: Message, state: FSMContext):
+    await state.set_state(Form.admin_start)  # состояние старта админа
+    await message.answer(f'Сейчас вы в меню, выберите действие', reply_markup=admin_start_kb)  # сообщение и подключение клавиатуры
--- resource/commands/admin_commands/patronymic_changing.py
+++ resource/commands/admin_commands/patronymic_changing.py
@@ -1,9 +1,12 @@
 from aiogram import F
+from ..states import Form
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from db.db_request.patronymic_changing_db import patronymic_changing_db
-from ..states import Form
+from ...keyboards.back_and_menu_button import back_and_menu_button
+from ...keyboards.admin_kb.admin_start_kb import admin_start_kb
+from ...keyboards.admin_kb.changing_personal_data_kb import changing_personal_data_kb
 
 
 router = Router()
@@ -24,3 +27,16 @@ async def process_new_patronymic(message: Message, state: FSMContext):
     new_first_patronymic = message.text.strip()
     patronymic_changing_db(new_first_patronymic, username)
     await message.answer(f"Отчество изменено на: {new_first_patronymic}")
+    await state.set_state(Form.back_and_menu)
+    await message.answer("Выберите действие", reply_markup=back_and_menu_button)
+
+
+@router.message(F.text == 'Назад', Form.back_and_menu)
+async def back_button(message: Message, state: FSMContext):
+    await state.set_state(Form.changing_personal_data)
+    await message.answer(f'Меню изменения персональных данных', reply_markup=changing_personal_data_kb)
+
+@router.message(F.text == 'В меню', Form.back_and_menu)
+async def menu_button(message: Message, state: FSMContext):
+    await state.set_state(Form.admin_start)  # состояние старта админа
+    await message.answer(f'Сейчас вы в меню, выберите действие', reply_markup=admin_start_kb)  # сообщение и подключение клавиатуры
